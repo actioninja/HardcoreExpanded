9d5d75721b30c7d5b16014aa65a513ca6c057191
actioninja <actioninja@gmail.com>
2017-04-24 21:22:34 -0700

    Update Build Script

--- src/main/java/werty/softhardcore/main/References.java
+++ src/main/java/werty/softhardcore/main/References.java
@@ -7,7 +7,7 @@ public class References
 {
     public static final String MODID = "softhardcore";
     public static final String NAME = "Soft Hardcore";
-    public static final String VERSION = "1.10.2-1.1.0";
+    public static final String VERSION = "${version}";
 
     public static final List<String> AUTHORS = Arrays.asList("TheWerty1124", "actioninja");
 
